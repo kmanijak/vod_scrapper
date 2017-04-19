2d857c50cd46104976311a199e9bcdc8a46dc0a1
Karol Manijak <karol.manijak@grandparade.co.uk>
2017-04-19 12:27:27 +0200

    Added .editoconfig

--- index.js
+++ index.js
@@ -4,19 +4,18 @@ const app = express();
 const port = 3000;
 
 app.get('/', (request, response) => {
-    response.send('Hello from Express!')
+  response.send('Hello from Express!')
 });
 
 app.use((err, request, response, next) => {
-    console.log('Error', err);
-    response.status(500).send('Something broke!')
+  console.log('Error', err);
+  response.status(500).send('Something broke!');
 });
 
 
 app.listen(port, (err) => {
-    if (err) {
-        return console.log('something bad happened', err)
-    }
-
-    console.log(`server is listening on ${port}`)
+  if (err) {
+    return console.log('something bad happened', err);
+  }
+  console.log(`server is listening on ${port}`);
 });
